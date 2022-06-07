519ec1fb8e80cf1c9b44cff19eade9e665b46588
Julissy Tocachelo <jutocachelo@gmail.com>
2022-06-07 00:29:25 -0300

    apagar arquivo

--- perguntasAdcCerveja.js
+++
@@ -1,9 +0,0 @@
-let readLine = require('readline-sync');
-
-let perguntas = {
-    nome: readLine.question('Qual o nome da cerveja? '),
-    tipo: readLine.question('Qual o tipo de cerveja? '),
-    nota: readLine.question('Qual a sua nota para essa cerveja?')
-}
-
-module.exports = perguntas
